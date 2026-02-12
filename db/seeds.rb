59418b48f28d856f4e674f8b9240e7a3c22e0f77
Diogo Tanaka <diogohideki@gmail.com>
2026-02-12 20:47:09 -0300

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,7 +12,7 @@ m2 = Movie.create!(title: "The Shawshank Redemption", overview: "Framed in the 1
 m3 = Movie.create!(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
 m4 = Movie.create!(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
 
-List.create!(name: "My favorite movies")
+l1 = List.create!(name: "My favorite movies")
 
 Bookmark.create!(list: l1, movie: m1, comment: "Muito bom")
 Bookmark.create!(list: l1, movie: m3, comment: "Também bom")
