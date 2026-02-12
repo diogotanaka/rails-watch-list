041c496838e521b770d4e42b8ab3b6c26804fe6c
Diogo Tanaka <diogohideki@gmail.com>
2026-02-12 00:19:53 -0300

    watch-list

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,38 @@
+class ListsController < ApplicationController
+  before_action :set_list, only: %i[show edit destroy]
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    @list.save
+    redirect_to lists_path(@list), notice: "List created successfully."
+  end
+
+  def edit
+  end
+
+  def destroy
+    @list.destroy
+    redirect_to lists_path, status: :see_other
+  end
+
+private
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+end
